9c135219e94d4a1264bf5051dc54051e142960eb
maxxwell131_W530 <maxxwell131@yahoo.com>
2018-08-25 17:14:11 +0300

    lesson 27 commit

---
+++ RunGame/circle.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace RunGame
+{
+    class Circle
+    {
+        public Point center { get; private set; }
+        public int radius { get; private set; }
+        public Color color { get; private set; }
+
+        public Circle( int x, int y, int radius)
+        {
+            center = new Point( x, y);
+            this.radius = radius;
+            color = Color.Blue;
+        }
+    }
+}
